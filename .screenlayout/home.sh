ff194ef037b19a62d21222f4933b32e3ed2719c3
Vern Hockney <fvhockney@gmail.com>
2019-08-07 10:51:13 +0200

    update home i3 layout

--- .screenlayout/home.sh
+++ .screenlayout/home.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output DP3 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output eDP1 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP2 --off
+xrandr --output HDMI1 --primary --mode 1920x1080 --pos 0x200 --rotate normal --output DP1 --mode 1280x1024 --pos 3286x0 --rotate left --output eDP1 --mode 1366x768 --pos 1920x512 --rotate normal --output VIRTUAL1 --off
